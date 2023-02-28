0ab0e8f6b13c3bcdaf518aab26ccf6ea1bc797c8
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-02-28 11:51:15 +0300

    Daire Çizme Projesi Tamamlandı

---
+++ Actions.cs
@@ -0,0 +1,22 @@
+namespace C__Projeleri__Daire_Çizme;
+public class Actions
+{
+    public void Daire(int radius)
+    {
+        for (double i = -radius; i < radius; i++)
+        {
+            for (double j = -radius; j < radius; j += 0.5)
+            {
+                if ((j * j) + (i * i) >= radius * radius)
+                {
+                    Console.Write("-");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
